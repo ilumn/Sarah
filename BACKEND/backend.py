d710fd702364c48021e1c8d88304e3f45ae23535
Nolempk <42507764+ilumn@users.noreply.github.com>
2022-07-08 16:53:59 -0400

    backend update

--- BACKEND/backend.py
+++ BACKEND/backend.py
@@ -1,6 +1,34 @@
-from flask import Flask
+from flask import Flask, g
 from flask import request
- 
+import openai
+import keyboard
+import json
+
+#api key
+key = 'api-key'
+
+#openai api request
+openai.api_key = key
+openai.Answer()
+def main(payload):
+  global answer
+  response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=payload,
+    temperature=0.7,
+    max_tokens=64,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+  resp = str(response)
+  answer = resp[resp.index("\"text\": \"")+9:resp.index("\"",resp.index("\"text\": \"")+9)]
+  answer = answer.replace("\\n","")
+  return answer
+  
+
+
+#flask server
 app=Flask(__name__)
 
 @app.route('/')
@@ -11,7 +39,8 @@ def blank():
 @app.route('/post',methods=["POST"])
 def post():
     payload=request.form['payload']
-    return payload
+    main(payload)
+    return answer
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
